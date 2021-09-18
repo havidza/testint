e20d316abe71bc7e7ffc0fdc9a7bb2ba6faad49e
havidza <zaenialmuttaqien@gmail.com>
2021-09-18 14:43:14 +0700

    awkokwkw

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php 
+
+ // $nilai = 56;
+
+ // if ($nilai < 70) {
+ // 	echo "Maaf anda harus remidi";
+ // } elseif ($nilai < 80) {
+ // 	echo "Selamat anda mendapatkan nilai C";
+ // } elseif ($nilai < 90) {
+ // 	echo "Selamat anda mendapatkan nilai B";
+ // } else {
+ // 	echo "Selamat anda mendapatkan nilai A";
+ // }
+ 
+// //Fizzbuzz
+// for ($i=1; $i < 30; $i++) { 
+// 	if ($i % 3 == 0 && $i % 5 == 0) {
+// 		echo "FizzBuzz <br>";
+// 	} elseif ($i % 3 == 0) {
+// 		echo "Fizz<br>";
+// 	} elseif ($i % 5 == 0) {
+// 		echo "Buzz<br>";
+// 	} else {
+// 		echo $i . "<br>";
+// 	}
+// }
+
+ ?>
